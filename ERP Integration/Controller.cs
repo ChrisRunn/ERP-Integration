4a01387efdf178188a77abf51eb2cda48641ee2f
ChrisRunn <christian.runnstrom@gmail.com>
2014-04-23 11:09:12 +0200

    Hej

--- ERP Integration/Controller.cs
+++ ERP Integration/Controller.cs
@@ -15,13 +15,14 @@ namespace ERP_Integration
             dal = new DataAccessLayer();
         }
 
+        #region Select, Update, Delete, Insert
+
         public DataSet GetAllEmployees()
         {
             return dal.GetAllEmployees();
         }
 
-
-       /* public void InsertEmployee(string no, string name, string lastName)
+        public void InsertEmployee(string no, string name, string lastName)
         {
             this.dal.InsertEmployee(no, name, lastName);
         }
@@ -34,6 +35,68 @@ namespace ERP_Integration
         public string UpdateEmployee(string no, string name, string lastName)
         {
             return this.dal.UpdateEmployee(no, name, lastName);
-        }*/
+        }
+
+        #endregion Select, Update, Delete, Insert
+
+        #region Uppgift A
+        public DataSet GetEmployeeAndMetadata()
+        {
+            return dal.GetEmployeeAndMetadata();
+        }
+
+        public DataSet GetEmployeeAndRelatives()
+        {
+            return dal.GetEmployeeAndRelatives();
+        }
+
+        public DataSet GetSickEmployee()
+        {
+            return dal.GetSickEmployee();
+        }
+
+        public DataSet GetMostSickEmployee()
+        {
+            return dal.GetMostSickEmployee();
+        }
+
+        #endregion Uppgift A
+
+        #region Uppgift B
+        public DataSet GetAllKeys()
+        {
+            return dal.GetAllKeys();
+        }
+
+        public DataSet GetAllIndexes()
+        {
+            return dal.GetAllIndexes();
+        }
+
+        public DataSet GetAllConstraints()
+        {
+            return dal.GetAllConstraints();
+        }
+
+        public DataSet GetAllTables()
+        {
+            return dal.GetAllTables();
+        }
+
+        public DataSet GetAllTables2()
+        {
+            return dal.GetAllTables2();
+        }
+
+        public DataSet GetColumnsEmployee()
+        {
+            return dal.GetColumnsEmployee();
+        }
+
+        public DataSet GetColumnsEmployee2()
+        {
+            return dal.GetColumnsEmployee2();
+        }
+        #endregion Uppgift B
     }
 }
--- ERP Integration/DataAccessLayer.cs
+++ ERP Integration/DataAccessLayer.cs
@@ -9,14 +9,90 @@ namespace ERP_Integration
 {
     class DataAccessLayer
     {
-
         Service1SoapClient proxy = new Service1SoapClient();
-        
-        
-        public DataSet GetAllEmployees(){
+
+        #region Select, Update, Delete, Insert
+        public void InsertEmployee(string no, string name, string lastName)
+        {
+            return proxy.InsertEmployee(no, name, lastName);
+        }
+
+        public string DeleteEmployee(string no)
+        {
+            return proxy.DeleteEmployee(no);
+        }
+
+        public string UpdateEmployee(string no, string name, string lastName)
+        {
+            return proxy.UpdateEmployee(no, name, lastName);
+        }
+
+        public DataSet GetAllEmployees()
+        {
             return proxy.GetAllEmployees();
         }
 
+        #endregion Select, Update, Delete, Insert
+
+        #region Uppgift A
+        public DataSet GetEmployeeAndMetadata()
+        {
+            return proxy.GetEmployeeAndMetadata();
+        }
+
+        public DataSet GetEmployeeAndRelatives()
+        {
+            return proxy.GetEmployeeAndRelatives();
+        }
+
+        public DataSet GetSickEmployee()
+        {
+            return proxy.GetSickEmployee();
+        }
+
+        public DataSet GetMostSickEmployee()
+        {
+            return proxy.GetMostSickEmployee();
+        }
+
+        #endregion Uppgift A
+
+        #region Uppgift B
+        public DataSet GetAllKeys()
+        {
+            return proxy.GetAllKeys();
+        }
+
+        public DataSet GetAllIndexes()
+        {
+            return proxy.GetAllIndexes();
+        }
+
+        public DataSet GetAllConstraints()
+        {
+            return proxy.GetAllConstraints();
+        }
+
+        public DataSet GetAllTables()
+        {
+            return proxy.GetAllTables();
+        }
+
+        public DataSet GetAllTables2()
+        {
+            return proxy.GetAllTables2();
+        }
+
+        public DataSet GetColumnsEmployee()
+        {
+            return proxy.GetColumnsEmployee();
+        }
+
+        public DataSet GetColumnsEmployee2()
+        {
+            return proxy.GetColumnsEmployee2();
+        }
+        #endregion Uppgift B
 
 
     }
